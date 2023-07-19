84ace1604bef17caf9a87b5f5a83c21221bbccbf
Shubham <shubhamkhale301@gmail.com>
2023-07-19 13:44:39 +0530

    added chat box component

--- src/app/pages/home/chat/chat.page.ts
+++ src/app/pages/home/chat/chat.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  
+  name: string = 'Sender';
+  message: string;
+  isLoading = false;
+  currentUserId = 1;
+  chats = [
+    {id: 1, sender: 1, message: 'hi'},
+    {id: 2, sender: 2, message: 'hi there!'},
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  sendMessage() {}
 
 }
